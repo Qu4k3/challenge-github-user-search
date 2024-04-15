89ef6131a4961644ef2ea646316255651e539b55
Qu4k3 <paul.guillamon@gmail.com>
2024-04-15 20:49:45 +0200

    ADD 700 font weight

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "@fontsource/space-mono"
+import '@fontsource/space-mono/400.css';
+import '@fontsource/space-mono/700.css';
 import './index.scss'
 import App from './App.jsx'
 
